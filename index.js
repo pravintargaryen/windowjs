ff2fa0c31c388e53edbec9f1028d02e22cd5b5a9
pravintargaryen <96950453+pravintargaryen@users.noreply.github.com>
2023-07-18 11:52:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const h = document.querySelector(".heading")
 const p = document.querySelector(".para")
 
-window.addEventListener("resize", {
+
+
+window.addEventListener("resize", () => {
   h.innerHTML = `<h1>Your Screen Resolution is</h1>`
   p.innerHTML = window.innerWidth + " X " + window.innerHeight
 } )
