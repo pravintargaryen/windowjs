7ca8e272c77a3e7308c63a69c771bf62e904c098
pravintargaryen <96950453+pravintargaryen@users.noreply.github.com>
2023-07-19 11:44:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const h = document.querySelector(".heading")
 const p = document.querySelector(".para")
 
+window.addEventListener("load", () => {
+  h.textContent = "Your Viewport Resolution is"
+  p.textContent = window.innerWidth + " X " + window.innerHeight
+  
+})
+
 window.addEventListener("resize", () => {
   h.textContent = "Your Screen Resolution is"
   p.textContent = window.innerWidth + " X " + window.innerHeight
